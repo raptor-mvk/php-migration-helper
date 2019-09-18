4109cf1d23e8621f62b5ab67e86936b28658854b
raptor_MVK <m.v.kamorin@gmail.com>
2019-09-18 18:16:03 +0300

    Add acceptable versions for doctrine and symfony packages

--- src/VersionComparator/VersionComparator.php
+++ src/VersionComparator/VersionComparator.php
@@ -17,7 +17,7 @@ use Composer\Semver\Comparator;
  *
  * @copyright 2019, raptor_MVK
  */
-class VersionComparator implements VersionComparatorInterface
+final class VersionComparator implements VersionComparatorInterface
 {
     /**
      * Returns compatibility report in a line-per-element array.
@@ -33,15 +33,47 @@ class VersionComparator implements VersionComparatorInterface
         foreach ($installedVersions as $installedVersion) {
             if (isset($installedVersion['name'], $installedVersion['version'])) {
                 $package = $installedVersion['name'];
-                $requiredPackageVersion = $requiredVersions[$package] ?? '0.0';
-                $installedPackageVersion = $this->normalizeVersion($installedVersion['version']);
-                if (Comparator::greaterThan($requiredPackageVersion, $installedPackageVersion)) {
-                    $result[] = "Update $package at least to {$requiredVersions[$package]}";
+                $requiredVersion = $requiredVersions[$package] ?? '0.0';
+                $reportLine = $this->getReportLine($package, $requiredVersion, $installedVersion['version']);
+                if (null !== $reportLine) {
+                    $result[] = $reportLine;
                 }
             }
         }
 
         return (1 === count($result)) ? [] : $result;
+    }
+
+    /**
+     * Returns _true_ if version is real version and not a warning message and _false_ otherwise.
+     *
+     * @param string $version
+     *
+     * @return bool
+     */
+    public function isRealVersion(string $version): bool
+    {
+        return 1 === preg_match('/^\d+(\.\d+(\.\d+)?)?$/', $version);
+    }
+
+    /**
+     * Returns report line for package if necessary or null otherwise.
+     *
+     * @param string $package
+     * @param string $requiredVersion
+     * @param string $installedVersion
+     *
+     * @return string|null
+     */
+    private function getReportLine(string $package, string $requiredVersion, string $installedVersion): ?string
+    {
+        $installedVersion = $this->normalizeVersion($installedVersion);
+        if (!$this->isRealVersion($requiredVersion)) {
+            return $requiredVersion;
+        }
+
+        return Comparator::greaterThan($requiredVersion, $installedVersion) ?
+            "Update $package at least to {$requiredVersion}" : null;
     }
 
     /**
--- tests/data/command/installed.tst
+++ tests/data/command/installed.tst
@@ -1,10 +1,102 @@
 [
   {
+    "name": "cloudinary/cloudinary_php",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/annotations",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/cache",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/collections",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/common",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/data-fixtures",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/dbal",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/doctrine-bundle",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/doctrine-cache-bundle",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/doctrine-fixtures-bundle",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/orm",
+    "version": "1.0"
+  },
+  {
+    "name": "doctrine/persistence",
+    "version": "1.0"
+  },
+  {
     "name": "phpunit/phpunit",
-    "version": "7.0.5"
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/http-client",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/mime",
+    "version": "1.0"
   },
   {
     "name": "symfony/monolog-bundle",
-    "version": "2.8.3"
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/phpunit-bridge",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-intl-idn",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-mbstring",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-php56",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-php70",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-php72",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/polyfill-util",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/swiftmailer-bundle",
+    "version": "1.0"
+  },
+  {
+    "name": "symfony/symfony",
+    "version": "1.0"
   }
 ]
